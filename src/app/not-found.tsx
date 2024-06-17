8d384ff3c76f4b0ab9ab61ed74f982ad4e1ab2f1
Jeel <baimsnik@gmail.com>
2024-06-17 23:25:57 +0530

    small fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
 			<div className='bg-background shadow-md rounded-lg p-6 max-w-lg text-center'>
 				<h1 className='text-3xl font-bold text-red-600'>Oops!</h1>
 				<p className='mt-4 text-lg text-muted-foreground'>
-					Something went wrong. We can't find the page you're looking for.
+					Something went wrong. We can&apos;t find the page you&apos;re looking for.
 				</p>
 				<div className='mt-6'>
 					<Link href='/'>Go to Home</Link>
